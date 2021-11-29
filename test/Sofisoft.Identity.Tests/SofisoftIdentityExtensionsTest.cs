34a20dc6e1f7dedf2230314181b0e8b72a66f314
Juan Manuel Abanto <94788498+juanmabam@users.noreply.github.com>
2021-11-28 23:08:38 -0500

    add test to class extensions

--- test/Sofisoft.Identity.Tests/SofisoftIdentityExtensionsTest.cs
+++ test/Sofisoft.Identity.Tests/SofisoftIdentityExtensionsTest.cs
@@ -1,5 +1,9 @@
 using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Sofisoft.Abstractions.Managers;
+using Sofisoft.Identity.Managers;
 using Xunit;
 
 namespace Sofisoft.Identity.Tests
@@ -45,7 +49,49 @@ namespace Sofisoft.Identity.Tests
 
             // Assert
             Assert.Equal("configuration", exception.ParamName);
+        }
 
+        [Theory]
+        [InlineData(typeof(IHttpContextAccessor), typeof(HttpContextAccessor))]
+        [InlineData(typeof(IIdentityManager), typeof(IdentityManager))]
+        public void AddIdentity_register_types(Type serviceType, Type implementationType)
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            var builder = new SofisoftBuilder(services);
+
+            // Act
+            builder.AddIdentity();
+
+            // Assert
+            Assert.Contains(services, service => service.ServiceType == serviceType &&
+                                                service.ImplementationType == implementationType);
+        }
+
+        [Fact]
+        public void AddIdentity_set_options()
+        {
+            // Arrange
+            var services = CreateServices();
+            var builder = CreateBuilder(services);
+
+            // Act
+            builder.AddIdentity(options =>
+            {
+                options.SetClientIdKey("client");
+                options.SetCompanyIdKey("company");
+                options.SetUserIdKey("id");
+                options.SetUsernameKey("name");
+            });
+
+            var provider = services.BuildServiceProvider();
+            var options = provider.GetRequiredService<IOptionsMonitor<SofisoftIdentityOptions>>();
+
+            // Assert
+            Assert.Equal("client", options.CurrentValue.ClientIdKey);
+            Assert.Equal("company", options.CurrentValue.CompanyIdKey);
+            Assert.Equal("id", options.CurrentValue.UserIdKey);
+            Assert.Equal("name", options.CurrentValue.UsernameKey);
         }
 
         private static SofisoftBuilder CreateBuilder(IServiceCollection services)
